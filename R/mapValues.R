967198607bf8a211f80c06e5ab4622037738b0ad
Murat Tasan <murat.tasan@sympoz.com>
2016-03-17 21:53:24 -0600

    altered mapValue behavior slightly to deal with lists more generally

--- R/mapValues.R
+++ R/mapValues.R
@@ -1,10 +1,15 @@
-## Basically a drop-in replacement for Hadley's plyr::mapvalues with different behavor for missing values
-## If `missing_from` is `NULL` and `x` contains values not found in `from`, those unfound values remain unchanged (apart from possible coersion rules to the mode of `to`).
-## If `missing_from` is non-`NULL` and `x` contains values not found in `from`, those unfound values are replaced with `missing_from`.
-mapValues <- function(x, from, to, missing_from = NA) {
+## Very similar in principle to Hadley's plyr::mapvalues with different behavor for missing values.
+##
+## If `keep_x_when_missing_from` == TRUE, then:
+##   (1) `missing_from_val` is ignored;
+##   (2) For positions `iix` where `x` is not found in `from`, `to[iix] <- x[iix]`.
+## If `keep_x_when_missing_from` == FALSE, then:
+##   (1) If `to` is atomix, then `missing_from_val` must be a scalar;
+##   (2) If `to` is a list (or object), `missing_from_val` can be anything that would otherwise fit into each position of `to`.
+mapValues <- function(x, from, to, keep_x_when_missing_from = FALSE, missing_from_val = NA) {
     if(length(from) != length(to)) stop("`from` and `to` vectors are not the same length.")
     if(!is.atomic(x)) stop("`x` must be an atomic vector.")
-    if(length(missing_from) != 1) stop("`missing_from` must be a scalar.")
+    if(!keep_x_when_missing_from && is.atomic(to) && length(missing_from_val) != 1) stop("`missing_from_val` must be a scalar when `to` is an atomic vector")
 
     if(is.factor(x)) {
         levels(x) <- mapValues(levels(x), from, to, missing_from)
@@ -14,10 +19,10 @@ mapValues <- function(x, from, to, missing_from = NA) {
     from_to_iix <- match(x, from) ## length(from_to_iix) == length(x)
     unfound_x_lix <- is.na(from_to_iix) ## length(unfound_x_lix) == length(x)
     RV <- to[from_to_iix] ## length(RV) == length(x)
-    if(!is.null(missing_from)) {
-        RV[unfound_x_lix] <- missing_from
-    } else { ## missing_from == NULL and thus pass unmapped `x` values through to the result
+    if(keep_x_when_missing_from) {
         RV[unfound_x_lix] <- x[unfound_x_lix]
+    } else {
+        RV[unfound_x_lix] <- missing_from_val
     }
     RV
 } 
